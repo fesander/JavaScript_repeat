7e71fe4e05f96c5ab6e90c704328dcd997598852
Fedor <fesander@mail.ru>
2018-11-03 20:40:31 +0300

    Small updates

--- Chess/chess.js
+++ Chess/chess.js
@@ -21,6 +21,7 @@ function matrixArray(rows,columns){
 }
 
 let fullTableMap = matrixArray(numberOfCells,numberOfCells);
+let possibleStepsTableMap = matrixArray(numberOfCells,numberOfCells);
 
 function resetTable() {
     while (table.firstChild) {
@@ -32,6 +33,8 @@ function resetTable() {
             ((i + j) % 2 == 0) ? tableCell.classList.add('white-cell') : tableCell.classList.add('black-cell');
             tableCell.classList.add('cell-' + i + '-' + j);
             table.appendChild(tableCell);
+            tableCell.classList.remove('chess-piece');
+            tableCell.classList.remove('possible-step');
         }
     }
 }
@@ -46,8 +49,17 @@ function displayCurrentPosition() {
                 tableCell.classList.add(fullTableMap[i][j].color);
                 tableCell.addEventListener('click', takePeace);
             }
-            else {
-                tableCell.addEventListener('click', nextStep);
+            tableCell.addEventListener('click', nextStep);
+        }
+    }
+}
+
+function displayPossibleSteps() {
+    for ( let i = 0; i < numberOfCells ; i++) {
+        for (let j = 0; j < numberOfCells; j++) {
+            tableCell = document.getElementsByClassName('cell-' + i + '-' + j)[0];
+            if (possibleStepsTableMap[i][j] != null) {
+                tableCell.classList.add('possible-step');
             }
         }
     }
@@ -57,10 +69,13 @@ function takePeace() {
     if (this.classList.contains('take-piece')) {
         this.classList.remove('take-piece');
         choosePiece = false;
+        resetTable();
+        displayCurrentPosition();
     }
     else if (!choosePiece){
         this.classList.add('take-piece');
         choosePiece = true;
+        possibleMove(this.classList);
     }
 }
 
@@ -76,6 +91,7 @@ function nextStep() {
             if (previousCellClassList[i].match(/cell-\d-\d$/i)) {
                 yP = previousCellClassList[i].split('-')[2];
                 xP = previousCellClassList[i].split('-')[1];
+                break;
             }
         }
 
@@ -84,15 +100,55 @@ function nextStep() {
             if (currentCellClassList[i].match(/cell-\d-\d$/i)) {
                 yC = currentCellClassList[i].split('-')[2];
                 xC = currentCellClassList[i].split('-')[1];
+                break;
             }
         }
-        fullTableMap[xC][yC] = fullTableMap[xP][yP];
-        fullTableMap[xP][yP] = null;
-        choosePiece = false;
-        resetTable();
-        displayCurrentPosition();
+
+        if (!(xC === xP && yC === yP)) {
+            fullTableMap[xC][yC] = fullTableMap[xP][yP];
+            fullTableMap[xP][yP] = null;
+            choosePiece = false;
+            resetTable();
+            displayCurrentPosition();
+        }
     }
 }
+
+function possibleMove(currentCellClassList) {
+    console.log("PossibleMove executed:\n" + currentCellClassList);
+    let yC,
+        xC;
+    for (let i = 0; i < currentCellClassList.length; i++) {
+        if (currentCellClassList[i].match(/cell-\d-\d$/i)) {
+            yC = +currentCellClassList[i].split('-')[2];
+            xC = +currentCellClassList[i].split('-')[1];
+            break;
+        }
+    }
+    switch (fullTableMap[xC][yC].piece){
+        case 'pawn':
+            possibleMovePawn(xC, yC);
+            break;
+        default:
+            console.log("Неизвестная фигура");
+    }
+    displayPossibleSteps();
+    console.log(xC + "\n" + yC);
+}
+
+function possibleMovePawn(x , y) {
+    let direction = 1;
+    if(fullTableMap[x][y].color == 'chess-piece-white') {
+        direction = -1;
+    }
+    console.log(1 * direction);
+    console.log(x);
+    console.log((1 * direction) + x);
+    possibleStepsTableMap[x + (1 * direction)][y] = true;
+    if(x == 6 && direction == -1 || x == 1 && direction == 1)
+        possibleStepsTableMap[x + (2 * direction)][y] = true;
+}
+
 function getInitialPosition() {
     for ( let i = 0; i < numberOfCells ; i++) {
         for (let j = 0; j < numberOfCells; j++ ) {
